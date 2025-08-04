f7af65315e6c0e17f457af69a44a80ad3c7a6eda
Sahil Satija <96464952+codebysahil@users.noreply.github.com>
2025-08-04 19:05:32 +0530

    Update iife.js

--- iife.js
+++ iife.js
@@ -3,6 +3,8 @@
 // also a no name function
 // no external name
 // not a reusable function
+// cannot assigned an external name
+// can return something
 // parameters can be passed
 (function(){
     console.log("Start the server");
@@ -17,3 +19,17 @@
 
     console.log("start the server .."+ servername);
 })('tomcat');
+
+
+
+
+let empDetails=(function(empName){
+
+    console.log(empName+ " is the employee name");
+    return empName.toUpperCase();
+})('suboh');
+
+console.log(empDetails);
+
+// here empDetails is not a functional expression
+
